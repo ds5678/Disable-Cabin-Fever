f4848ec368f6d5a25bd203051490f7e5574815e9
WulfMarius <WulfMarius@users.noreply.github.com>
2018-12-23 09:33:16 +0100

    updated for TLD V1.44

--- src/Implementation.cs
+++ src/Implementation.cs
@@ -1,12 +1,27 @@
 ﻿using UnityEngine;
+using System.Reflection;
 
 namespace DisableCabinFever
 {
     internal class Implementation
     {
+        public const string NAME = "Disable-Cabin-Fever";
+
         public static void OnLoad()
         {
-            Debug.Log("[Disable-Cabin-Fever]: Version " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+            AssemblyName assemblyName = Assembly.GetExecutingAssembly().GetName();
+            Log("Version " + assemblyName.Version);
+        }
+
+        internal static void Log(string message)
+        {
+            Debug.LogFormat("[" + NAME + "] {0}", message);
+        }
+
+        internal static void Log(string message, params object[] parameters)
+        {
+            string preformattedMessage = string.Format("[" + NAME + "] {0}", message);
+            Debug.LogFormat(preformattedMessage, parameters);
         }
     }
 }
